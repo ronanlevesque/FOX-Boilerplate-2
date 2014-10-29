7a181694731d7e46c99d9c3a2f347dc36343a491
eskiiss <ronan.levesque@gmail.com>
2014-10-29 18:04:34 +0100

    Refactoring gulpfile + package

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,34 @@
 // Plugins
 
-var gulp = require('gulp'),
-    connect = require('gulp-connect'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minify = require('gulp-minify-css'),
-    csslint = require('gulp-csslint'),
-    htmlhint = require('gulp-htmlhint'),
-    jshint = require('gulp-jshint'),
-    imagemin = require('gulp-imagemin'),
-    svg2png = require('gulp-svg2png'),
-    svgmin = require('gulp-svgmin'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    notify = require('gulp-notify');
+var gulp = require('gulp');
+var $$ = require('gulp-load-plugins')();
 
 // Server
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'dev',
-    port: 1337,
-    livereload: true
-  });
+// gulp.task('connect', function() {
+//   $$.connect.server({
+//     root: 'dev',
+//     port: 1337,
+//     livereload: true
+//   });
+// });
+
+gulp.task('connect', function () {
+  var connect = require('connect');
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(serveStatic('dev'))
+    .use(serveStatic('.tmp'))
+    .use(serveIndex('dev'));
+
+  require('http').createServer(app)
+    .listen(1337)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
 });
 
 // Handle CLI errors
@@ -35,8 +41,8 @@ function handleError(err) {
 // Folders
 
 var baseFolder = {
-  dev  : './dev/',
-  dist : './dist/'
+  dev  : 'dev/',
+  dist : 'dist/'
 };
 
 var devFolder = {
@@ -54,82 +60,78 @@ var distFolder = {
 // Dev tasks
 
 gulp.task('sass', function() {
-  return gulp.src([devFolder.css + 'sass/**/*.scss'])
-  .pipe(sass())
-  .on('error', notify.onError("Error: <%= error.message %>"))
+  return gulp.src(devFolder.css + 'sass/**/*.scss')
+  .pipe($$.sass())
+  .on('error', $$.notify.onError("Error: <%= error.message %>"))
   .on('error', handleError)
-  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe($$.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(gulp.dest(devFolder.css))
-});
-
-gulp.task('html', function () {
-  return gulp.src(baseFolder.dev + '**/*.html')
-  .pipe(connect.reload());
-});
-
-gulp.task('css', function () {
-  return gulp.src(devFolder.css + '**/*.css')
-  .pipe(connect.reload());
 });
 
 gulp.task('scripts', function() {
   return gulp.src([devFolder.js + '**/*.js', '!' + devFolder.js + 'ie/*.js', '!' + devFolder.js + '**/main.js'])
-  .pipe(concat('main.js'))
+  .pipe($$.concat('main.js'))
   .pipe(gulp.dest(devFolder.js))
-  .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
+
+  $$.livereload.listen();
+
+  gulp.watch([
+    devFolder.css + '**/*.css',
+    devFolder.js + '**/*.js',
+    baseFolder.dev + '**/*.html'
+  ]).on('change', $$.livereload.changed);
+
   gulp.watch(devFolder.css + '**/*.scss', ['sass']);
-  gulp.watch(devFolder.css + '**/*.css', ['css']);
   gulp.watch(devFolder.js + '**/*.js', ['scripts']);
-  gulp.watch(baseFolder.dev + '**/*.html', ['html']);
 });
 
 // Testing tasks
 
 gulp.task('csslint', function() {
   return gulp.src(devFolder.css + '**/*.css')
-  .pipe(csslint())
-  .pipe(csslint.reporter('default'))
+  .pipe($$.csslint())
+  .pipe($$.csslint.reporter('default'))
 });
 
 gulp.task('htmlhint', function() {
   return gulp.src(baseFolder.dev + '**/*.html')
-  .pipe(htmlhint())
-  .pipe(htmlhint.reporter('default'))
+  .pipe($$.htmlhint())
+  .pipe($$.htmlhint.reporter('default'))
 });
 
 gulp.task('jshint', function() {
   return gulp.src(devFolder.js + '**/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'))
+  .pipe($$.jshint())
+  .pipe($$.jshint.reporter('default'))
 });
 
 // Deploy tasks
 
 gulp.task('minify', function() {
   return gulp.src(devFolder.css + '**/*.css')
-  .pipe(minify())
+  .pipe($$.minifyCss())
   .pipe(gulp.dest(distFolder.css))
 });
 
 gulp.task('uglify', function() {
   return gulp.src(devFolder.js + 'main.js')
-  .pipe(uglify())
+  .pipe($$.uglify())
   .pipe(gulp.dest(distFolder.js))
 });
 
 gulp.task('img', function() {
   return gulp.src(devFolder.img + '**/*')
-  .pipe(imagemin())
+  .pipe($$.imagemin())
   .pipe(gulp.dest(distFolder.img))
 });
 
 gulp.task('svg', function () {
   return gulp.src(devFolder.img + '**/*.svg')
-  .pipe(svgmin())
-  .pipe(svg2png())
+  .pipe($$.svgmin())
+  .pipe($$.svg2png())
   .pipe(gulp.dest(distFolder.img));
 });
 
@@ -142,7 +144,7 @@ gulp.task('copy', function () {
 
 gulp.task('servermsg', function () {
   return gulp.src(baseFolder.dev)
-  .pipe(notify({
+  .pipe($$.notify({
     title: '[TASK] -- Local server',
     message: 'Local server created at localhost:1337.'
   }));
@@ -150,7 +152,7 @@ gulp.task('servermsg', function () {
 
 gulp.task('testmsg', function () {
   return gulp.src(baseFolder.dev)
-  .pipe(notify({
+  .pipe($$.notify({
     title: '[TASK] -- Testing files',
     message: 'Tests run on your CSS, HTML and JS files. See console for more details.'
   }));
@@ -158,7 +160,7 @@ gulp.task('testmsg', function () {
 
 gulp.task('deploymsg', function () {
   return gulp.src(baseFolder.dev)
-  .pipe(notify({
+  .pipe($$.notify({
     title: '[TASK] -- Deploy',
     message: 'Files successfully deployed under /dist.'
   }));
